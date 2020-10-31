233bfd8b97fd2c46ce67dffcf30eada3d670f81c
Fernando Bustamante <fernando.miguel.bustamante@gmail.com>
2020-10-30 22:55:36 -0300

    añadido

--- src/App.js
+++ src/App.js
@@ -1,25 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
+import { data } from './data';
+import '@fortawesome/fontawesome' ;
+import '@fortawesome/fontawesome-free-solid' ;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <section className="hero is-primary">
+      <div className="hero-body">
+        <div className="container">
+          <h1 className="title">Hoteles</h1>
+          <h2 className="subtitle">
+            desde el <strong>dddd, DD de mmmm de AAAA</strong> hasta el <strong>dddd, DD de mmmm de AAAA</strong>
+          </h2>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default App;
